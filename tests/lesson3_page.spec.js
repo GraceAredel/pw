0355222c96253282a83c19fc6d64180998734e91
Valeriya Smirnova <valesmirnova@ozon.ru>
2024-07-22 16:51:41 +0300

    lesson 3 - expect

---
+++ tests/lesson3_page.spec.js
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('url and title', async ({ page }) => {
+  await page.goto('http://habr.com/ru');
+      
+  await expect(page).toHaveURL("https://habr.com/ru/feed/")
+  await expect(page).toHaveTitle("Публикации / Моя лента / Хабр")
+    
+});
